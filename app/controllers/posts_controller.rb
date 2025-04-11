83afffdb05ea10eba4c68018706fa300a4f3d8e9
Leon Shimizu <lmshimizu@gmail.com>
2025-04-11 19:58:14 +1000

    Add update and destroy functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   def create
     # commented out strong params for now
     # @post = Post.new(post_params)
-
+    puts "I am in the create action in the posts controller"
     @post = Post.new(
       title: params[:title],
       content: params[:content]
@@ -20,6 +20,23 @@ class PostsController < ApplicationController
     else
       render json: @post.errors, status: :unprocessable_entity # Send back errors with 422 status
     end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    @post.update(
+      title: params[:title] || @post.title,
+      content: params[:content] || @post.content
+    )
+
+    render json: @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render json: :no_content
   end
 
   # private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :create]
+  resources :posts, only: [:index, :create, :update, :destroy]
 end
